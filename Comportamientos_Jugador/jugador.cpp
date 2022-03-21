8f3e60a4bbc75f1b4698d6b5f1cfd817105bf823
pedro-jgl <pedrojgl14@gmail.com>
2022-03-21 12:04:59 +0100

    movido a wsl

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -101,7 +101,7 @@ Action ComportamientoJugador::think(Sensores sensores){
 	
 
 
-	//Decidir qué acción tomar de manera aleatoria
+	//Decidir qué acción tomar
 	girar_derecha = (rand()%2==0);
 	girar = rand() % 5;
 	entrar = rand() % 10;
